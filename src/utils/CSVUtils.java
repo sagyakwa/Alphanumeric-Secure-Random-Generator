f4bb3bbe4f7f758608de7d010badc7a44c408dea
Samuel Agyakwa <samuelagyen@gmail.com>
2020-02-08 22:16:05 -0500

    added CSVUtils class for CSV reading

---
+++ src/utils/CSVUtils.java
@@ -0,0 +1,119 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVUtils {
+    private static final char SEPARATOR_CHAR = ',';
+    private static final char QUOTE_CHAR = '"';
+    String csvFile;
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    public CSVUtils(String csvFile) {
+        this.csvFile = csvFile;
+    }
+
+//    public String readAllCSVLines() throws IOException {
+//
+////        String csvFile = "customers.csv";
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(this.csvFile))));
+//        String line;
+////        while ((line = reader.readLine()) != null) {
+////            List<String> lines = parseCSVLine(line);
+////            this.stringBuilder.append(lines.get(1));
+//////            System.out.println("Country [id= " + lines.get(0) + ", code= " + lines.get(1) + " , name=" + lines.get(2) + "]");
+////        }
+//        this.stringBuilder.append(Arrays.toString(new String[]{reader.readLine()}).replace(",", ""));
+//        this.stringBuilder.append(parseCSVLine(reader.readLine(), ','));
+//        reader.close();
+//
+//        return String.valueOf(this.stringBuilder);
+//    }
+
+    public static String parseCSVLine(String cvsLine) {
+        return String.valueOf(parseCSVLine(cvsLine, SEPARATOR_CHAR, QUOTE_CHAR));
+    }
+
+    public static List<String> parseCSVLine(String cvsLine, char separators) {
+        return parseCSVLine(cvsLine, separators, QUOTE_CHAR);
+    }
+
+    public static List<String> parseCSVLine(String cvsLine, char separators, char customQuote) {
+
+        List<String> result = new ArrayList<>();
+
+        //if empty, return!
+        assert cvsLine != null;
+
+        if (customQuote == ' ') {
+            customQuote = QUOTE_CHAR;
+        }
+
+        if (separators == ' ') {
+            separators = SEPARATOR_CHAR;
+        }
+
+        StringBuffer curVal = new StringBuffer();
+        boolean inQuotes = false;
+        boolean startCollectChar = false;
+        boolean doubleQuotesInColumn = false;
+
+        char[] chars = cvsLine.toCharArray();
+
+        for (char ch : chars) {
+            if (inQuotes) {
+                startCollectChar = true;
+                if (ch == customQuote) {
+                    inQuotes = false;
+                    doubleQuotesInColumn = false;
+                } else {
+                    //Fixed : allow "" in custom quote enclosed
+                    if (ch == '\"') {
+                        if (!doubleQuotesInColumn) {
+                            curVal.append(ch);
+                            doubleQuotesInColumn = true;
+                        }
+                    } else {
+                        curVal.append(ch);
+                    }
+                }
+            } else {
+                if (ch == customQuote) {
+
+                    inQuotes = true;
+
+                    //Fixed : allow "" in empty quote enclosed
+                    if (chars[0] != '"' && customQuote == '\"') {
+                        curVal.append('"');
+                    }
+
+                    //double quotes in column will hit this!
+                    if (startCollectChar) {
+                        curVal.append('"');
+                    }
+
+                } else if (ch == separators) {
+
+                    result.add(curVal.toString());
+
+                    curVal = new StringBuffer();
+                    startCollectChar = false;
+
+                } else if (ch != '\r') {
+                    if (ch == '\n') {
+                        //the end, break!
+                        break;
+                    } else {
+                        curVal.append(ch);
+                    }
+                }  //ignore LF characters
+            }
+        }
+
+        result.add(curVal.toString());
+
+        return result;
+    }
+
+}
