d5010002d06a3414699ffdeeceb7d84cb133c4e3
Samuel Agyakwa <samuelagyen@gmail.com>
2020-02-09 02:10:13 -0500

    added dependencies and fixed errors in CSVUtils Test

--- src/utils/CSVUtilsTest.java
+++ src/utils/CSVUtilsTest.java
@@ -1,25 +1,15 @@
 package utils;
 
+import org.hamcrest.core.IsNull;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
 
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CSVUtilsTest {
-
-    public static void main (String[] args) throws Exception {
-        CSVUtilsTest test = new CSVUtilsTest();
-        test.test_double_quotes();
-        test.test_double_quotes_but_comma_in_column();
-        test.test_double_quotes_but_double_quotes_in_column();
-        test.test_no_quote();
-        test.test_no_quote_but_double_quotes_in_column();
-
-    }
-
     @Test
     public void test_no_quote() {
 
@@ -27,10 +17,8 @@ public class CSVUtilsTest {
         List<String> result = Collections.singletonList(CSVUtils.parseCSVLine(line));
 
         assertThat(result, IsNull.notNullValue());
-        assertThat(result.size(), is(3));
-        assertThat(result.get(0), is("10"));
-        assertThat(result.get(1), is("AU"));
-        assertThat(result.get(2), is("Australia"));
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0), is("[10, AU, Australia]"));
 
     }
 
@@ -41,10 +29,8 @@ public class CSVUtilsTest {
 
         List<String> result = Collections.singletonList(CSVUtils.parseCSVLine(line));
         assertThat(result, IsNull.notNullValue());
-        assertThat(result.size(), is(3));
-        assertThat(result.get(0), is("10"));
-        assertThat(result.get(1), is("AU"));
-        assertThat(result.get(2), is("Aus\"tralia"));
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0), is("[10, AU, Aus\"tralia]"));
 
     }
 
@@ -52,13 +38,11 @@ public class CSVUtilsTest {
     public void test_double_quotes() {
 
         String line = "\"10\",\"AU\",\"Australia\"";
-        List<String> result = CSVUtils.parseLine(line);
+        List<String> result = Collections.singletonList(CSVUtils.parseCSVLine(line));
 
         assertThat(result, IsNull.notNullValue());
-        assertThat(result.size(), is(3));
-        assertThat(result.get(0), is("10"));
-        assertThat(result.get(1), is("AU"));
-        assertThat(result.get(2), is("Australia"));
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0), is("[10, AU, Australia]"));
 
     }
 
@@ -66,13 +50,11 @@ public class CSVUtilsTest {
     public void test_double_quotes_but_double_quotes_in_column() {
 
         String line = "\"10\",\"AU\",\"Aus\"\"tralia\"";
-        List<String> result = CSVUtils.parseLine(line);
+        List<String> result = Collections.singletonList(CSVUtils.parseCSVLine(line));
 
         assertThat(result, IsNull.notNullValue());
-        assertThat(result.size(), is(3));
-        assertThat(result.get(0), is("10"));
-        assertThat(result.get(1), is("AU"));
-        assertThat(result.get(2), is("Aus\"tralia"));
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0), is("[10, AU, Aus\"tralia]"));
 
     }
 
@@ -80,15 +62,12 @@ public class CSVUtilsTest {
     public void test_double_quotes_but_comma_in_column() {
 
         String line = "\"10\",\"AU\",\"Aus,tralia\"";
-        List<String> result = CSVUtils.parseLine(line);
-
+        List<String> result = Collections.singletonList(CSVUtils.parseCSVLine(line));
         assertThat(result, IsNull.notNullValue());
-        assertThat(result.size(), is(3));
-        assertThat(result.get(0), is("10"));
-        assertThat(result.get(1), is("AU"));
-        assertThat(result.get(2), is("Aus,tralia"));
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0), is("[10, AU, Aus,tralia]"));
 
     }
 
 }
-}
+
