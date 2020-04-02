7cd6d40d5645fd33103b1c6e6841fcb53498f6ca
Samuel Agyakwa <samuelagyen@gmail.com>
2020-04-01 22:00:52 -0400

    added test for CSV file without header

--- src/test/java/TransactionGeneratorTest.java
+++ src/test/java/TransactionGeneratorTest.java
@@ -20,6 +20,21 @@ public class TransactionGeneratorTest {
     }
 
     @Test
+    public void generateUsingOriginalCSVWithoutHeader() {
+        TransactionGenerator tr = new TransactionGenerator();
+        try {
+            long startTime = System.nanoTime();
+            System.out.println(tr.generateRandomIDs("src/main/java/customers.csv", false));
+            long endTime = System.nanoTime();
+            long elapsedTime = endTime - startTime;
+            double elapsedTimeInSeconds = (double) Long.parseLong(String.valueOf(elapsedTime)) / 1_000_000_000;
+            System.out.println(elapsedTime + " nanoseconds\n" + elapsedTimeInSeconds + " seconds");
+        } catch (IOException e) {
+            System.out.println("CSV file does not exist");
+        }
+    }
+
+    @Test
     public void generateUsingSampleCSV() {
         TransactionGenerator tr = new TransactionGenerator();
         try {
@@ -46,16 +61,6 @@ public class TransactionGeneratorTest {
             System.out.println(elapsedTime + " nanoseconds\n" + elapsedTimeInSeconds + " seconds");
         } catch (IOException e) {
             System.out.println("CSV file does not exist");
-        }
-    }
-
-    @Test
-    public void generateUsingTextFile() {
-        TransactionGenerator tr = new TransactionGenerator();
-        try {
-            System.out.println(tr.generateRandomIDs("src/main/java/Sample.txt"));
-        } catch(IOException e){
-            System.out.println("Doesn't exist");
         }
     }
 
